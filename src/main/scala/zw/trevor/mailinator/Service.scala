e0d3d6641552d1c6ab3fe9204ea6f31ed88723fb
Trevor Sibanda <sibandatrevor@gmail.com>
2018-11-25 10:57:34 +0200

    Define HTTP routes

--- src/main/scala/zw/trevor/mailinator/Service.scala
+++ src/main/scala/zw/trevor/mailinator/Service.scala
@@ -6,12 +6,21 @@ import org.http4s.HttpService
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 
+import MailBox._
+
 class Service[F[_]: Effect] extends Http4sDsl[F] {
+
+  def stub = Ok("TODO")
 
   val service: HttpService[F] = {
     HttpService[F] {
-      case GET -> Root / "hello" / name =>
-        Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+      case GET    -> Root/ "mailboxes"/ email / "messages" / messageId => stub
+      case GET    -> Root/ "mailboxes"/ email / "messages" / cursor / count => stub
+      case GET    -> Root/ "mailboxes"/ email / "messages" => stub
+      case POST   -> Root/ "mailboxes"   => stub
+      case POST   -> Root/ "mailboxes"/ email / "messages" => stub
+      case DELETE -> Root/ "mailboxes"/ email  => stub
+      case DELETE -> Root/ "mailboxes"/ email / "messages" / messageId => stub
     }
   }
 }
