6db49b2df8327aa23f6e13a392ae906685596556
Trevor Sibanda <sibandatrevor@gmail.com>
2018-11-25 11:44:07 +0200

    Make MailBox results mappable

--- src/main/scala/zw/trevor/mailinator/MailBox.scala
+++ src/main/scala/zw/trevor/mailinator/MailBox.scala
@@ -4,24 +4,25 @@ import MailBox._
 
 case class Email(val id: MailID, from: String, to: String, subject: String, body: String, received: Date)
 
-abstract class MailBox(){
+abstract class MailBox(val address: Address){
     //create a new email
-    def create(from: String, subject: String, body: String): Int
+    def create(from: Address, subject: String, body: String): Option[MailID]
 
     //fetch a particular email
-    def fetch(id: MailID): Email
+    def fetch(id: MailID): Option[Email]
 
     //fetch multiple email addresses
-    def fetchMultiple(page: Page): Iterable[Email]
+    def fetchMultiple(page: Page): PageResult[Email]
 
     //destroy this mailbox and all emails
     def purge: Unit
 
     //delete a particular email
-    def delete(id: MailID): Email
+    def delete(id: MailID): Option[Email]
 
 }
 
 object MailBox{
     type MailID = Int
+    type Address = String
 }
