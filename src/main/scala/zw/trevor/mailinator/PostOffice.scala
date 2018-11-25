e3794cb51be1a1c6740252a557c15ae5d52d801a
Trevor Sibanda <sibandatrevor@gmail.com>
2018-11-25 11:43:29 +0200

    IMplement PostOffice

--- src/main/scala/zw/trevor/mailinator/PostOffice.scala
+++ src/main/scala/zw/trevor/mailinator/PostOffice.scala
@@ -2,28 +2,46 @@ package zw.trevor.mailinator
 
 import MailBox._
 
-abstract class PostOffice(){
+abstract class PostOffice() extends Table[Address, MailBox]{
+    val tableImpl: Table[MailID, Email]
+
     //lookup a mailbox based on email address
-    def lookup(email: String): MailBox 
+    def lookup(address: Address): Option[MailBox] = this.get(address)
 
-    //create a new mailbox
-    def createMailBox: MailBox
+    //create a random mailbox  
+    def createRandom: Option[Address] = this.put(this.createMailBox)
+    
+    //fetch an email given its id
+    def fetch(id: MailID): Option[Email] = tableImpl.get(id)
+    
+    //fetch an email given its email and id. faster as it first look in the address mailbox
+    def fetch(address: Address)(id: MailID): Option[Email] = for{
+        mailbox <- this.lookup(address)
+        email <- mailbox.fetch(id)  
+    } yield email
 
-    //create a random mailbox
-    def createRandom: MailBox
+    //fetch multiple results given an address and a cursor
+    def fetchMultiple(address: Address)(page: Page): Option[PageResult[Email]] = for{
+        mailbox <- this.lookup(address)
+        results = mailbox.fetchMultiple(page)
+    } yield results
 
-    //fetch a mail given a mail id
-    def fetch(id: MailID): Email
+    //create a new email 
+    def createMail(to: Address)(from: Address,  subject: String, body: String): Option[MailID] = for{
+        mbox <- this.lookup(to)
+        mid  <- mbox.create(from, subject, body) 
+    } yield mid
 
-    //fetch multiple emails given a cursor
-    def fetchMultiple(addr: String)(page: Page): Iterable[Email]
+    //destroy a mailbox given an email address
+    def destroyBox(address: Address): Option[Address] = for{
+        mbox <- this.lookup(address)
+        _ = mbox.purge
+    } yield address
 
-    //create a new email
-    def createMail(addr: String)(to: String, subject: String, body: String): Email
+    //delete a particular email given its id. Does not perform mbox lookup
+    def deleteMail(id: MailID) = for{
+        email <- tableImpl.remove(id)
+        //misses in each mailbox will result in deletion
+    } yield id
 
-    //delete a particular email
-    def deleteMail(id: MailID): Email
-
-    //destroy an email
-    def destroyMail(addr: String): MailBox 
 }
